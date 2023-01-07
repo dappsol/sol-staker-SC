27b924d46373d67f393a70ec662fe427d9d35bb8
rabnail <gt2018acc@outlook.com>
2023-01-07 02:03:27 -0800

    updated

--- tests/deposit-game.ts
+++ tests/deposit-game.ts
@@ -5,12 +5,31 @@ import { DepositGame } from "../target/types/deposit_game";
 describe("deposit-game", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.getProvider();
+  let poolKeypair = anchor.web3.Keypair.generate();
 
   const program = anchor.workspace.DepositGame as Program<DepositGame>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const [
+        poolSigner,
+        nonce,
+    ] = await anchor.web3.PublicKey.findProgramAddress(
+        [
+          poolPubkey.toBuffer(),
+        ],
+        program.programId
+    );
+    
+    const tx = await program.methods.initialize(nonce).accounts({
+      authority: provider.wallet.publicKey,
+      pool: poolKeypair.publicKey,
+      poolSigner: poolSigner,
+      owner: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).signers([poolKeypair]).instructions([
+          await program.account.pool.createInstruction(poolKeypair, ),
+      ]).rpc();
     console.log("Your transaction signature", tx);
   });
 });
